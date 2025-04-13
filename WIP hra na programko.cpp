bbaea11778a2c509d1d6428e72cedc7a7edba406
ZuzeV2 <zuzana.varadiova@skolakrizik.cz>
2025-04-13 15:09:31 +0200

    Přidání základních proměnných
    Přidání základních proměnných na přípravu budoucího projektu.

---
+++ WIP hra na programko.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void utok(&utocnik, &cil){
+int poskozeni = rand()
+
+cout << jmeno << "zpusobil" << poskozeni << "poskozeni"<< endl;
+cout << enemyname << "má teï" << nepritelzivoty << endl;}
+
+
+int main(){
+    string jmeno, paladin, lovec, mag, warlock, enemyname, enemyname2, enemyname3;
+    float maxzivoty, zivoty, maxenergie, energie, penize, utok, nepritelzivoty;
+    int level;
+    float placeholderproutok;
+
+    hrac = {}
+//Min. poèet monster: 16
+//Min. poèet mini-bossù: 2
+//Min. poèet hlavních bossù: 1
+//Min. poèet vesnic: 2
+
+//Min. jedenkrát musí hráè bojovat proti: 1 monstru v jednom setkání.
+//Min. jedenkrát musí hráè bojovat proti: 2 monstrùm v jednom setkání.
+//Min. jedenkrát musí hráè bojovat proti: 3 monstrùm v jednom setkání.
+
+ }
