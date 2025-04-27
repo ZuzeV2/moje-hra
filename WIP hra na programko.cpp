e975455f1907454177943a3ed17b61333390b827
ZuzeV2 <zuzana.varadiova@skolakrizik.cz>
2025-04-27 12:14:22 +0200

    Přidání informací na jednotlivých klas
    Přidání statistiky jednotlivé klasy.

--- WIP hra na programko.cpp
+++ WIP hra na programko.cpp
@@ -9,12 +9,73 @@ cout << enemyname << "má teï" << nepritelzivoty << endl;}
 
 
 int main(){
-    string jmeno, paladin, lovec, mag, warlock, enemyname, enemyname2, enemyname3;
+    string jmeno, paladin, lovec, mag, warlock, enemyname, enemyname2, enemyname3, decision1, decision2 = "Ne", klasa;
     float maxzivoty, zivoty, maxenergie, energie, penize, utok, nepritelzivoty;
     int level;
     float placeholderproutok;
 
     hrac = {}
+
+    cout << "Jak se chcete jmenovat?: "
+    cin >> jmeno
+do{
+    cout << "Jakou klasu chcete? (Paladin, Lovec, Mag, Warlock): "
+    cin >> decision1
+
+    switch (decision1){
+case Paladin:
+    cout << "Zivoty: 5/5" << endl;
+    cout << "Utok: 3" << endl;
+    cout << "Mana: 5/5" << endl;
+    cout << "Utok - Uder za 3 poskozeni" << endl;
+    cout << "Uder svetlem - 1 poskozeni vsem nepratelum (stoji 2 many)" << endl;
+    cout << "Oziveni - Hrac si prida 1 zivot (nesmi prekrocit jeho maximalni zivot)(stoji 1 manu)" << endl;
+
+    cout << "Chcete si vybrat Paladina?"
+    cin >> decision2
+    If(decision2 == "Ano"){
+    klasa == paladin;}
+    else {}
+case Lovec:
+    cout << "Zivoty: 4/4" << endl;
+    cout << "Utok: 4" << endl;
+    cout << "Mana: 4/4" << endl;
+    cout << "Utok - Uder za 4 poskozeni" << endl;
+    cout << "Smrtici sip - Zasahne jednoho nepritele za 5 poskozeni (stoji 2 many)" << endl;
+    cout << "Past - Snizi utok vsech nepratel o 1 na jedno kolo (stoji 1 manu)" << endl;
+
+    cout << "Chcete si vybrat Lovce?"
+    cin >> decision2
+    If(decision2 == "Ano"){
+    klasa == lovec;}
+    else {}
+case Warlock:
+    cout << "Zivoty: 5/5" << endl;
+    cout << "Utok: 3" << endl;
+    cout << "Mana: 5/5" << endl;
+    cout << "Utok - Temny zasah za 3 poskozeni" << endl;
+    cout << "Krvavy ritual - Obetuje 1 zivot a doplni si 2 many" << endl;
+    cout << "Kletba - Udeli nepriteli 4 poskozeni behem dvou kol (stoji 2 many)" << endl;
+
+    cout << "Chcete si vybrat Warlocka?"
+    cin >> decision2
+    If(decision2 == "Ano"){
+    klasa == warlock;}
+    else {}
+case Mag:
+    cout << "Zivoty: 3/3" << endl;
+    cout << "Utok: 2" << endl;
+    cout << "Mana: 6/6" << endl;
+    cout << "Utok - Magicksy projektil za 2 poskozeni" << endl;
+    cout << "Ohniva koule - Zasahne jednoho nepritele za 6 poskozeni (stoji 3 many)" << endl;
+    cout << "Magicka bariera – Na 1 kolo zcela pohlti prvni utok, ktery by zasahl Maga (stoji 2 many)" << endl;
+
+    cout << "Chcete si vybrat Maga?"
+    cin >> decision2
+    If(decision2 == "Ano"){
+    klasa == mag;}
+    else {}
+}while (!(desicion2 == "Ano"))
 //Min. poèet monster: 16
 //Min. poèet mini-bossù: 2
 //Min. poèet hlavních bossù: 1
